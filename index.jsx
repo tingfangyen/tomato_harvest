9b52900d2c6f7520689181c834bd2117a801a11b
Ting-Fang Yen <ting-fangyen@MTF-MacBook-Air.local>
2026-08-31 07:39:50 -0700

    8/30 update

--- index.jsx
+++ index.jsx
@@ -39,6 +39,7 @@ const raw = [
   { day: "8/21", red: { weights: [], unweighed: 0 }, yellow: { weights: [13.35, 10.5], unweighed: 0 }},
   { day: "8/23", red: { weights: [], unweighed: 0 }, yellow: { weights: [15.7], unweighed: 0 }},
   { day: "8/27", red: { weights: [15.55], unweighed: 0 }, yellow: {weights: [], unweighed: 0 }},
+  { day: "8/30", red: { weights: [], unweighed: 0 }, yellow: { weights: [18.9, 17.55, 11.85, 12.3], unweighed: 0 }}, 
 ];
 
 const fmtLbOz = (oz) => {
